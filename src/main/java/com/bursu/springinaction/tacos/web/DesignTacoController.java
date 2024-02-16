3fe5384367ffbc9d6daaed72e69c5cd9db0c3008
Bogdan Ursu <bogdanionutursu@gmail.com>
2024-02-16 17:03:12 +0200

    Chapter 2 - adding validation

--- src/main/java/com/bursu/springinaction/tacos/web/DesignTacoController.java
+++ src/main/java/com/bursu/springinaction/tacos/web/DesignTacoController.java
@@ -4,9 +4,12 @@ import com.bursu.springinaction.tacos.Ingredient;
 import com.bursu.springinaction.tacos.Ingredient.Type;
 import com.bursu.springinaction.tacos.Taco;
 import com.bursu.springinaction.tacos.TacoOrder;
+import jakarta.validation.Valid;
+import lombok.Value;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
@@ -53,8 +56,11 @@ public class DesignTacoController {
     }
 
     @PostMapping
-    public String processTaco(Taco taco,
+    public String processTaco(@Valid Taco taco, Errors errors,
                               @ModelAttribute TacoOrder tacoOrder) {
+        if (errors.hasErrors()) {
+            return "design";
+        }
         tacoOrder.addTaco(taco);
         log.info("Processing taco: {}", taco);
         return "redirect:/orders/current";
