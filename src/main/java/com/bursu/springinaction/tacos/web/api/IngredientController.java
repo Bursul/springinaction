095750234bdf5b43783d8c6cb18123ff044d1e5f
Bogdan Ursu <bogdanionutursu@gmail.com>
2024-03-18 16:33:37 +0200

    Chapter 8 - Securing REST

---
+++ src/main/java/com/bursu/springinaction/tacos/web/api/IngredientController.java
@@ -0,0 +1,36 @@
+package com.bursu.springinaction.tacos.web.api;
+
+import com.bursu.springinaction.tacos.Ingredient;
+import com.bursu.springinaction.tacos.data.IngredientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path = "/api/ingredients", produces = "application/json")
+@CrossOrigin(origins = "http://localhost:2222")
+public class IngredientController {
+    private final IngredientRepository repo;
+
+    @Autowired
+    public IngredientController(IngredientRepository repo) {
+        this.repo = repo;
+    }
+
+    @GetMapping
+    public Iterable<Ingredient> allIngredients() {
+        return repo.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Ingredient saveIngredient(@RequestBody Ingredient ingredient) {
+        return repo.save(ingredient);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteIngredient(@PathVariable("id") String ingredientId) {
+        repo.deleteById(ingredientId);
+    }
+}
